70339bac493f34e0da4b750cb42b68c73daca825
blacksand <bblsand@outlook.com>
2025-07-02 04:31:56 +0800

    chore: update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,23 @@
 import { eslintConfig } from '@peaks/config-eslint'
 
-const config = eslintConfig({
-  type: 'app',
-  ignores: ['apps/**', 'packages/**', 'tools/**'],
-}, {
-  name: 'peaks/pnpm/rules/disable/root-package-json',
-  files: ['package.json'],
-  rules: {
-    'pnpm/json-enforce-catalog': 'off',
+const config = eslintConfig(
+  {
+    type: 'app',
+    ignores: ['apps/**', 'packages/**', 'tools/**', '.cursor/templates/**'],
+
+    formatters: {
+      css: true,
+      html: true,
+      markdown: 'prettier',
+    },
   },
-})
+  {
+    name: 'peaks/pnpm/rules/disable/root-package-json',
+    files: ['package.json'],
+    rules: {
+      'pnpm/json-enforce-catalog': 'off',
+    },
+  },
+)
 
 export default config
