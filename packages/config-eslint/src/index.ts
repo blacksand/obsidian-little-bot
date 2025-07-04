078788bc432dc7ce53fc9a1c008885535729d7b2
blacksand <bblsand@outlook.com>
2025-07-04 18:46:44 +0800

    chore(config-eslint): update eslint dependencies and exports
    - Add glob exports from `@antfu/eslint-config`.
    - Update `pnpm-workspace.yaml` to include new dependencies (`@radix-ui/react-dialog`, `cmdk`, etc.).
    - Adjust peer dependencies for `eslint-plugin-react` and `eslint-plugin-react-hooks`.

--- packages/config-eslint/src/index.ts
+++ packages/config-eslint/src/index.ts
@@ -1,3 +1,37 @@
 export { eslintConfig } from './lib/eslint-config.js'
+
 export { getProjectRoot } from './lib/utils/get-project-root.js'
 export { getTsconfigPath } from './lib/utils/get-tsconfig-path.js'
+
+export {
+  GLOB_ALL_SRC,
+  GLOB_ASTRO,
+  GLOB_ASTRO_TS,
+  GLOB_CSS,
+  GLOB_EXCLUDE,
+  GLOB_GRAPHQL,
+  GLOB_HTML,
+  GLOB_JS,
+  GLOB_JSON,
+  GLOB_JSON5,
+  GLOB_JSONC,
+  GLOB_JSX,
+  GLOB_LESS,
+  GLOB_MARKDOWN,
+  GLOB_MARKDOWN_CODE,
+  GLOB_MARKDOWN_IN_MARKDOWN,
+  GLOB_POSTCSS,
+  GLOB_SCSS,
+  GLOB_SRC,
+  GLOB_SRC_EXT,
+  GLOB_STYLE,
+  GLOB_SVELTE,
+  GLOB_SVG,
+  GLOB_TESTS,
+  GLOB_TOML,
+  GLOB_TS,
+  GLOB_TSX,
+  GLOB_VUE,
+  GLOB_XML,
+  GLOB_YAML,
+} from '@antfu/eslint-config'
