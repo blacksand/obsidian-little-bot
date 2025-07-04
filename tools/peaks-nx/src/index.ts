ec800d7f020a46205d1f22731c9d4c468edd8ebf
blacksand <bblsand@outlook.com>
2025-07-04 16:34:21 +0800

    refactor(peaks-nx): rename `library-react` generator to `library-ui`
    - Update generator name and paths in `nx.json`, `generators.json`, and related files.
    - Adjust exports and file structure to reflect the new naming convention.

--- tools/peaks-nx/src/index.ts
+++ tools/peaks-nx/src/index.ts
@@ -1,2 +1,2 @@
 export { libJsGenerator } from './generators/library-js/generator.js'
-export { libReactGenerator } from './generators/library-react/generator.js'
+export { libReactGenerator } from './generators/library-ui/generator.js'
