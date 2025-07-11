880d3394cbb2d683fe3bcc801ee48a3f4aeb1a9c
blacksand <bblsand@outlook.com>
2025-07-12 03:39:15 +0800

    chore: remove `tslog`, and update type usage in settings

--- packages/core/src/lib/settings/little-bot-settings.ts
+++ packages/core/src/lib/settings/little-bot-settings.ts
@@ -1,5 +1,7 @@
 import { Effect, Ref } from 'effect'
 
+import type { Undefinable } from '@peaks/utils'
+
 import { Uninitialized } from '../errors/uninitialized'
 import { sanitizeSettings } from './sanitized-settings'
 import type { SanitizedSettings } from './sanitized-settings'
@@ -8,7 +10,7 @@ export class LittleBotSettings extends Effect.Service<LittleBotSettings>()(
   '@peaks/core/LittleBotSettings',
   {
     effect: Effect.gen(function* () {
-      const settingsRef = yield* Ref.make<SanitizedSettings | undefined>(undefined)
+      const settingsRef = yield* Ref.make<Undefinable<SanitizedSettings>>(undefined)
 
       const getAll = () => settingsRef.pipe(
         Effect.flatMap((settings) =>
